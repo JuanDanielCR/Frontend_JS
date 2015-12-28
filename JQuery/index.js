5daa6336b9a69b8eb2a0c4f3019397c511f2a84d
JuanDanielCR <castilloreyesjuan@gmail.com>
2015-12-27 22:17:07 -0600

    Diversas formas de iniciar Jquery

--- JQuery/index.js
+++ JQuery/index.js
@@ -1,1 +1,17 @@
-alert("Hola JS");
+//Se ejecuta antes que:
+/*
+	window.onload=function(){
+		alert("Hola");
+	}
+
+	$ es una variable que guarda la funcion jQuery(), esta a su vez recibe varios parametros entre ellos esta document.
+	Existen diversas formas de iniciar el código js.
+	
+	$(document).ready(function(){.....})
+	$().ready(function(){.............})
+	$(function(){.....................})
+	Estos tres códigos realizan la misma accion
+*/
+$(document).ready(function(){
+	alert("hola");
+})
