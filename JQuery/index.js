a191414b88f7ab88514f5b72f72886be5fcecb11
JuanDanielCR <castilloreyesjuan@gmail.com>
2015-12-27 23:27:39 -0600

    Selectores Jquery, con css y contexto

--- JQuery/index.js
+++ JQuery/index.js
@@ -13,9 +13,7 @@
 	jQuery(document).ready(function(){.......})
 	Estos cuatro códigos realizan la misma accion
 */
-$(document).ready(function(){
-	alert("hola");
-})
+
 /*
 	PROTOTYPE	
 	Prototype es un framework escrito en JavaScript que se orienta al desarrollo sencillo y dinámico de aplicaciones web.
@@ -29,3 +27,39 @@ $(document).ready(function(){
 	...})
 	$ //Este $ es de prototype
 */
+$(document).ready(function(){
+
+	/*
+	SELECTORES:
+		Cuando $('') recibe un string busca estos caracteres en el DOM, por lo tanto a este String se le conoce como selector.
+		Los selectores de JQUERY son iguales a los selectores css, asi para obtener un hijo,
+		en vanillaJS tendría que guardar al padre y luego acceder al hijo pero con jquery solo usaria sintaxis de selector css
+		var hijo=$('h1 +h2');
+
+	var hijo; 
+	Es un objeto de JQuery, es decir ya no es el elemento del DOM en sí.
+
+	El elemento del DOM con VanillaJS
+	var header= document.getElementById('app-header');
+	*/
+
+	var header= $('header');
+	console.log(header);
+
+	//Todos los elementos que sean h1 o h2
+	var headings= $('h1,h2');
+
+	/*
+		Ademas de usar selectores de css ppuedo mandar arrays para buscar, pseudoclases y usar contextos
+	*/
+
+	/*
+		Contextos:
+		hijos_header es JQUERY OBJECT que guardara a todos los h1 que esten dentro un contexto header[0], 
+		recordar que var header es un objeto de JQuery, por lo tanto si lo quiero usar como contexto de bo poner [0]
+		para hacer referencia al objeto de DOM! 
+		Así se buscaran a los h1 que esten dentro del header del DOM y no Object Jquery
+	*/
+	var hijos_header=$('h1',header[0]);
+	console.log(hijos_header);
+})
