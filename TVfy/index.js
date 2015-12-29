58d68550a73966cc55539b0a663d809c53e70a2b
JuanDanielCR <castilloreyesjuan@gmail.com>
2015-12-29 13:35:27 -0600

    Agregando AJAX a tvfy y realizando el append de los htmls bindenados con los JSON obtenidos

--- TVfy/index.js
+++ TVfy/index.js
@@ -1,70 +1,62 @@
-$(function() {
-  var $tvShowsContainer = $('#app-body').find('.tv-shows');
+$(document).ready(function(){
+  $("#app-body")
+  .find("form")
+  .submit(function(event){
+    event.preventDefault();
+    var cadena_busqueda=$(this).find("input").val();
+    alert("Se ha buscado "+cadena_busqueda);
+  })
 
-  function renderShows(shows) {
-    $tvShowsContainer.find('.loader').remove();
-    shows.forEach(function (show) {
-      var article = template
-        .replace(':name:', show.name)
-        .replace(':img:', show.image.medium)
-        .replace(':summary:', show.summary)
-        .replace(':img alt:', show.name + " Logo")
+  /*
+    String_Templates
+    :name:
+    Me sirven para despues reemplazarso por lo valores de JSON Object
 
-      var $article = $(article)
-      $article.hide();
-      $tvShowsContainer.append($article.show());
-    })
-  }
-
-  /**
-   * Submit serach form
-   */
-
-  $('#app-body')
-    .find('form')
-    .submit(function (ev) {
-      ev.preventDefault();
-      var busqueda = $(this)
-        .find('input[type="text"]')
-        .val();
-
-      $tvShowsContainer.find('.tv-show').remove()
-      var $loader = $('<div class="loader">');
-      $loader.appendTo($tvShowsContainer);
-      $.ajax({
-        url: 'http://api.tvmaze.com/search/shows',
-        data: { q: busqueda },
-        success: function (res, textStatus, xhr) {
-          $loader.remove();
-          var shows = res.map(function (el) {
-            return el.show;
-          })
-
-          renderShows(shows);          
-        }
-      })
-    })
-
+  */
   var template = '<article class="tv-show">' +
           '<div class="left img-container">' +
-            '<img src=":img:" alt=":img alt:">' +
+            '<img src=":img:" alt=":img_alt:">' +
           '</div>' +
           '<div class="right info">' +
             '<h1>:name:</h1>' +
             '<p>:summary:</p>' +
           '</div>' +
         '</article>';
+  /*
+    AJAX como $ es un funcion esta puede recibir funciones como ajax()
+    La funcion ajax(url,setting)
+    url = string
+    settings = objeto {clave:valor}
 
-  if (!localStorage.shows) {
-    $.ajax('http://api.tvmaze.com/shows')
-      .then(function (shows) {
-        $tvShowsContainer.find('.loader').remove();
-        localStorage.shows = JSON.stringify(shows);
-        renderShows(shows);
-      })
-  } else {
-    renderShows(JSON.parse(localStorage.shows));
-  }
-  
+    Ejemplo:
+      $.ajax("http://api.tvmaze.com/shows",{:value})
+      También puedo poner la url como parametro del objeto
+  */
+    $.ajax({
+      url:"http://api.tvmaze.com/shows",
+      success:function(data,textStatus,xhr){
+
+        var container= $("#app-body").find('.tv-shows');
+
+        //data es un array de JSON Objects, usar .forEach()
+        data.forEach(function(item){
+          /*
+            MANIPULAR JSON
+            item.value : Paraa acceder al valor de una llave en JSON usar el nombre del objeto punto y el nombre de la clave
+            var a = objeto.clave
+            a - guarda el valor de la clave
+          */
+
+          //Uso del metodo replace() que actua sobre Strings
+
+          var template_bind =
+          template.replace(":name:",item.name)
+                  .replace(":summary:",item.summary)
+                  .replace(":img:",item.image.medium)
+                  .replace(":img_alt:", item.name+" Logo")
+
+         container.append($(template_bind));
+        })
+      }
+    })
 })
-
