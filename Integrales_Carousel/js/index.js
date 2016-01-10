5134963280c09e1f1baf018c38d39dfc3476832a
JuanDanielCR <castilloreyesjuan@gmail.com>
2016-01-09 20:56:33 -0600

    Agregando estilos basicos

--- Integrales_Carousel/js/index.js
+++ Integrales_Carousel/js/index.js
@@ -1,3 +1,23 @@
 $(document).ready(function(){
-	console.log("Jquery ready");
+/*Scroll*/
+    $('a[href^="#"]').on('click',function (e) {
+        e.preventDefault();
+
+        var target = this.hash;
+        var $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 1200, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+/*Menu agrega clases*/
+	var items_menu=$('.menu').find('li');
+	items_menu.on('click',function(){
+		items_menu.removeClass("active");
+		$( this ).toggleClass( "active" );
+		$("window").scrollTop($("*:contains('Mexico'):eq(n)").offset().top);
+	});
+
 });
