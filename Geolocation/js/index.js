eb83167d40f56b57abd5c72b80db7a4e5c8c8393
JuanDanielCR <castilloreyesjuan@gmail.com>
2016-01-19 09:58:25 -0600

    Geolocation

--- Geolocation/js/index.js
+++ Geolocation/js/index.js
@@ -1,26 +1,41 @@
-(function() {
+/*Funcion autoejecutable o closure, se ejecuta en cuanto se carga el archivo*/
+(function(){
+  /*
+  GEOLOCALIZACION con VanillaJS
 
-  if(navigator.geolocation) {
+  navigator: Objeto del BOM
+  .geolocation: funcion del objeto navigator
+  
+  1. Validar que nuestro navegador soporte al objeto navigator
 
-    navigator.geolocation.getCurrentPosition(getCoords, errorFound);
+  */
 
-  } else {
-    alert("Por favor, actualiza tu navegador");
+  if(navigator.geolocation){
+    /*
+      2.Al objeto navigator aplicar la function geolocation y aplicarle otro metodo que es getCurrentPosition
+      el cual recibe un callback para success y otro para error.
+    */
+    navigator.geolocation.getCurrentPosition(f_success,f_error);
+  }else{
+    alert("Actualiza tu navegador");
   }
 
+  function f_error(error){
+    alert("Error de tipo: "+error);
+    /*
+    Errores:
+    0-Desconocido
+    1-Acceso denegado
+    2-Posicion no disponible
+    3-Tiemout
+    */
+  }
 
-  function errorFound(error) {
-    alert("Un error ocurrió: " + error.code);
-    // 0: Error desconocido
-    // 1: Permiso denegado
-    // 2: Posición no está disponible
-    // 3: Timeout
-  };
-
-  function getCoords(position) {
+  function f_success(position){
     var lat = position.coords.latitude;
     var lon = position.coords.longitude;
-    console.log("Tu posición es: " + lat + "," + lon);
+
+    alert("Lat: "+lat+" Long: "+lon);
   }
 
 })();
