695b53a743dacf6658360ac3befae74962118d5d
JuanDanielCR <castilloreyesjuan@gmail.com>
2016-01-20 17:59:38 -0600

    Creando array de celdas y materias

--- Scheduler/js/app.js
+++ Scheduler/js/app.js
@@ -11,5 +11,41 @@
             window.location.hash = target;
         });
     });
-	    
+
+	/*LOGICA*/
+
+	/*Objeto JSON para un horario*/
+	var horario = [];
+	crearHorario();
+
+	function crearHorario(){
+		for(var i=0;i<75;i++){
+			horario.push({
+				"id_celda":i,
+				"ocupada":false,
+				"materias":[]
+			});
+		}
+	}
+
+	function agregarMateria(id,name,prof,punt){
+		var celda=horario[id];
+		celda.ocupada=true;
+		celda.materias.push({
+			"nombre":name,
+			"profesor":prof,
+			"puntuacion":punt
+		});
+	}
+
+	function borrarMateria(name,prof){
+
+	}
+
+	agregarMateria(9,"Algebra","Neumann",10);
+	agregarMateria(9,"IES","Adriana",6);
+	
+	var celda_vista=horario[9];
+	console.log(celda_vista);
+
 })();
