f6f346ac70d020c4dbea75e017e685b8ceea2af8
JuanDanielCR <castilloreyesjuan@gmail.com>
2016-01-20 15:46:13 -0600

    Rebuild with wood

--- Scheduler/js/app.js
+++ Scheduler/js/app.js
@@ -1,4 +1,15 @@
 (function(){
-	console.log("JS");
-	
+	    $('a[href^="#"]').on('click',function (e) {
+        e.preventDefault();
+
+        var target = this.hash;
+        var $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 1200, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+	    
 })();
