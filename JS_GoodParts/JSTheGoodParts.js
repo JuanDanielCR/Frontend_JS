40c166bac8efcd7d8430543290c4889f9a296283
JuanDanielCR <castilloreyesjuan@gmail.com>
2016-02-05 22:09:05 -0600

    Callbacks non J

--- JS_GoodParts/JSTheGoodParts.js
+++ JS_GoodParts/JSTheGoodParts.js
@@ -327,3 +327,12 @@ var myObject = function(){
 myObject.show();
 myObject.increment(2);
 
+/*
+	CALLBACKS
+	Funciones que se pasan como parametros para ejecutar acciones no sincronicas
+*/
+
+var bodys = document.getElementsByTagName("body");
+bodys.addEventListener('click',function(){
+	console.log("Ouch");
+})
