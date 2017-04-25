cd0093a6cfa73faa86dd25c478ae8e911babf3a1
yope <castillodasdas>
2017-04-25 07:33:51 -0500

    El boton shido de nates no sirve

--- WebSQL/js/index.js
+++ WebSQL/js/index.js
@@ -68,9 +68,8 @@ var formularioFotos = "<div class='related'>"+
 						"<h4>Sección: <select name='' id='sltCarpeta'>"+
 							"{options}"+
 						"</select></h4>"+
-							"<input type='file' name='file-1[]' id='flFoto' class='inputfile inputfile-1' data-multiple-caption='{count} archivos'  accept='image/*' multiple required/>"+
-							"<label for='file-1'><svg xmlns='http://www.w3.org/2000/svg' width='20' height='17' viewBox='0 0 20 17'><path d='M10 0l-5.2 4.9h3.3v5.1h3.8v-5.1h3.3l-5.2-4.9zm9.3 11.5l-3.2-2.1h-2l3.4 2.6h-3.5c-.1 0-.2.1-.2.1l-.8 2.3h-6l-.8-2.2c-.1-.1-.1-.2-.2-.2h-3.6l3.4-2.6h-2l-3.2 2.1c-.4.3-.7 1-.6 1.5l.6 3.1c.1.5.7.9 1.2.9h16.3c.6 0 1.1-.4 1.3-.9l.6-3.1c.1-.5-.2-1.2-.7-1.5z'/></svg> <span>Foto</span></label>"+
-						"<h4>Notas: </h4>"+
+							"<input type='file'  id='flFoto' data-multiple-caption='{count} archivos'  accept='image/*' multiple required/>"+
+							"<h4>Notas: </h4>"+
 						"<textarea name=' id=' cols='25' rows='5' id='txtNota'></textarea>"+
 						"<input type='submit' value='Subir' id='formFotos'>"+
 					"</form>"+
@@ -97,8 +96,7 @@ var formularioCarpetas = "<div class='related'>"+
 						"<h4>Área:</h4>"+
 						 "<input type='text' >"+
 						"<h4>Imágen:</h4>"+
-						"<input type='file' name='file-1[]' id='file-1' class='inputfile inputfile-1' data-multiple-caption='{count} archivos'  accept='image/*' multiple required/>"+
-							"<label for='file-1'><svg xmlns='http://www.w3.org/2000/svg' width='20' height='17' viewBox='0 0 20 17'><path d='M10 0l-5.2 4.9h3.3v5.1h3.8v-5.1h3.3l-5.2-4.9zm9.3 11.5l-3.2-2.1h-2l3.4 2.6h-3.5c-.1 0-.2.1-.2.1l-.8 2.3h-6l-.8-2.2c-.1-.1-.1-.2-.2-.2h-3.6l3.4-2.6h-2l-3.2 2.1c-.4.3-.7 1-.6 1.5l.6 3.1c.1.5.7.9 1.2.9h16.3c.6 0 1.1-.4 1.3-.9l.6-3.1c.1-.5-.2-1.2-.7-1.5z'/></svg> <span>Logo</span></label>"+
+						"<input type='file' id='file-1' data-multiple-caption='{count} archivos'  accept='image/*' multiple required/>"+
 							"<br>"+
 						"<input type='submit' value='Crear'>"+
 					"</form>"+
